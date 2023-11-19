923cd0e0f8ed6c48bf68049c882903e937d034f2
sandeepmulla <151423420+sandeepmulla@users.noreply.github.com>
2023-11-19 11:23:03 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,15 @@
+let login = document.getElementById("login-sub");
+console.log(login);
+
+login.onclick = () =>{
+    let form=document.getElementById("form-1")
+    console.log(form);
+
+    let display = form.classlist.toggle{
+        if (display) {
+            form.style.display = "block"
+
+        }
+       
+    }
+}
